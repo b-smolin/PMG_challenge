dc5e196dbe6b64b26ff07ba41e9bb1e9309e5314
Ben Smolin <tud18885@temple.edu>
2023-02-06 20:00:24 -0500

    added some tests, pushing to setup CI/CD testing

--- combine.py
+++ combine.py
@@ -13,22 +13,22 @@ def validateInput(args: List[str]) -> List[str]:
     '''Takes a list of arguments and validates it against requirements.
     Program must be called with >=  1 arguments from command line
      and each argument should be the path of a CSV file as str'''
-    try:
-        if len(args) == 1:
-            raise ArgumentException("Too few arguments. Expected >= 1")
-        cleaned = args[1:]
-        for filename in cleaned:
-            if not os.path.isfile(filename):
-                raise IOError("Couldn't find file " + filename)
-            if len(filename) > 4 and filename[-4:] != ".csv":
-                raise TypeError("File" + filename +
-                                "does not appear to be a csv")
+    # try:
+    if len(args) == 1:
+        raise ArgumentException("Too few arguments. Expected >= 1")
+    cleaned = args[1:]
+    for filename in cleaned:
+        if not os.path.isfile(filename):
+            raise IOError("Couldn't find file " + filename)
+        if len(filename) > 4 and filename[-4:] != ".csv":
+            raise TypeError("File" + filename +
+                            "does not appear to be a csv")
 
         return cleaned
 
-    except (ArgumentException, IOError, TypeError) as e:
-        print(e)
-        sys.exit(2)
+    # except (ArgumentException, IOError, TypeError) as e:
+    #     print(e)
+    #     # sys.exit(2)
 
 
 def getFileName(s: str) -> str:
---
+++ test_combine.py
@@ -0,0 +1,39 @@
+from combine import getFileName, validateInput, ArgumentException
+import pytest
+
+
+def test_name_stripping1():
+    name = "\\data\\household_cleaners.csv"
+    assert getFileName(name) == "household_cleaners.csv"
+
+
+def test_name_stripping2():
+    name = "\\data\\clothing.csv"
+    assert getFileName(name) == "clothing.csv"
+
+
+def test_name_stripping3():
+    name = "\\data\\files\\items\\contents\\IMPORTANTDATA.csv"
+    assert getFileName(name) == "IMPORTANTDATA.csv"
+
+
+def test_good_args1():
+    args = [".\\combine.py", ".\\data\\accessories.csv"]
+    args = validateInput(args)
+    assert len(args) == 1 and args == [".\\data\\accessories.csv"]
+
+
+def test_good_args2():
+    args = [".\\combine.py", ".\\data\\accessories.csv",
+            ".\\data\\clothing.csv",
+            ".\\data\\household_cleaners.csv"]
+    args = validateInput(args)
+    assert len(args) == 3 and args == [".\\data\\accessories.csv",
+                                       ".\\data\\clothing.csv",
+                                       ".\\data\\household_cleaners.csv"]
+
+
+def test_too_few_args():
+    args = [".\\combine.py"]
+    with pytest.raises(ArgumentException):
+        validateInput(args)
